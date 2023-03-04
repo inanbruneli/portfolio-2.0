98559c014b5e1797b6c86888c728ea92c6b7bcef
inanbruneli-home <58962462+inanbruneli@users.noreply.github.com>
2023-03-03 23:33:09 -0300

    skills done

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,6 +1,18 @@
 import './index.scss';
+import { skills } from '../../assets/skills';
 
 export default function Skills() {
+  function makeStars(star: number) {
+    let retorno = new Array;
+    for (let i = 0; i < 5; i++) {
+      const classe = i < star ? 'fill' : '';
+      retorno.push(<div className={`dot ${classe}`}></div>)
+    }
+    return retorno;
+  }
+
+  const aplicacoes = ['frontend', 'backend', 'UI'];
+
   return (
     <div className="container-skills col-12 mt-5">
       <div className="sobre-mim">
@@ -12,40 +24,22 @@ export default function Skills() {
       </div>
 
       <div className="mt-3 d-flex justify-content-between">
-        <div className="skill-item">
-          <h1>frontend</h1>
-          <div className="list">
-            <div className='d-flex justify-content-between'>
-              <h2>Javascript</h2>
-              <div className="dots d-flex col-5 justify-content-between">
-                <div className="dot fill"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-              </div>
-            </div>
-            <div className='d-flex justify-content-between'>
-              <h2>Javascript</h2>
-              <div className="dots d-flex col-5 justify-content-between">
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-              </div>
+        {aplicacoes.map(aplicacao => (
+          <div className="skill-item">
+            <h1>{aplicacao}</h1>
+            <div className="list mt-4">
+              {skills.filter(skill => skill.tipo == aplicacao).map(skill => (
+                <div className='d-flex justify-content-between mb-1'>
+                  <h2>{skill.linguagem}</h2>
+                  <div className="dots d-flex col-5 justify-content-between">
+                    {makeStars(skill.estrelas)}
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
-        </div>
-        <div className="skill-item">
-          <h1>backend</h1>
-        </div>
-        <div className="skill-item">
-          <h1>ui/ux</h1>
-        </div>
+        ))}
       </div>
-
-
     </div>
   )
 }
